bc40781738932703f3ff14afa69fd08a2e6f3d96
Joedidi <352514951@qq.com>
2019-11-08 13:58:46 +0800

    xiugai

---
+++ utils/fdfs/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author: Jamin Chen
+@date:  2019/11/7 10:17
+@explain: 
+@file: __init__.py.py
+"""
---
+++ utils/fdfs/storage.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author: Jamin Chen
+@date:  2019/11/7 10:17
+@explain: 
+@file: storage.py
+"""
+
+from django.core.files.storage import Storage
+from fdfs_client.client import *
+from django.conf import settings
+
+class FDFSStorage(Storage):
+    """fast_DFS文件存储类"""
+    def __init__(self, client_conf=None, base_url=None):
+        if client_conf is None:
+            client_conf = settings.FDFS_CLIENT_CONF
+        if base_url is None:
+            base_url = settings.FDFS_URL
+
+        self.client_conf = client_conf
+        self.base_url = base_url
+
+    def _open(self, name, mode='rb'):
+        """打开文件时使用"""
+        pass
+
+    def _save(self, name, content):
+        """保存文件时使用"""
+        # name:你选择上传的文件的名字
+        # content:包含你上传的文件内容的File对象
+        client_conf_obj = get_tracker_conf(self.client_conf)
+        client = Fdfs_client(client_conf_obj)
+        res = client.upload_by_buffer(content.read())
+        if res.get('Status') != 'Upload successed.':
+            # 上传失败
+            raise Exception('上传文件到fast_DFS失败')
+
+        # 获取上传文件返回的id
+        file_id = res.get('Remote file_id').decode()
+        return file_id
+
+        # #创建一个对象
+        # trackers = get_tracker_conf('D:/dailyfresh/utils/fdfs/client.conf')
+        # # client = Fdfs_client('./utils/fdfs/client.conf')
+        # client = Fdfs_client(trackers)
+        # #上传到系统中
+        # res = client.upload_appender_by_buffer(content.read())
+        # if res.get('Status') != 'Upload successed.':
+        #     # 上传失败
+        #     raise Exception('上传文件到fast_DFS失败')
+        # # 获取上传文件返回的id
+        # file_id = res.get('Remote file_id').decode()
+        # return file_id
+
+    def exists(self, name):
+        # 返回name是否存在
+
+        return False
+
+    def url(self, name):
+        return self.base_url + name
